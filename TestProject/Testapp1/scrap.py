b02324eab1aa7f6f00eef7a3980c25b6d9a8c1ab
abhishekpattnaik <abhishekpattnaik06@gmail.com>
2020-01-02 23:33:02 +0530

    new commit4

--- TestProject/Testapp1/scrap.py
+++ TestProject/Testapp1/scrap.py
@@ -1,24 +1,20 @@
-from bs4 import BeautifulSoup
-import requests
-# url = input("url")
-# url ='https://elemental.medium.com/science-confirms-that-the-vagus-nerve-is-key-to-well-being-c23fab90e211' 
-# res = requests.get(url)
-# soup = BeautifulSoup(res.text,'html.parser')
-# for elem in soup.find_all('button'):
-# 	if elem.string.endswith('claps'):
-# 		claps = elem.string
-# 		break;
-# heading = soup.find('h1').string
-# count =0
-# for i in  soup.find_all('h2'):
-# 	count = count+1
-# 	if(count==4):
-# 		author=i.string
-# val = {"author":author,"claps":claps,"heading":heading}
-# print(val)
-# from pymongo import MongoClient
-# client = MongoClient()
-# db = client.testdatabase
-# collection = db.collection
-# t=collection.insert_one(val)  
-# print(t)
+from helper import *
+# from urls import *
+
+def urlList():
+	urls = []
+	number_of_urls=int(input("no of urls"))
+	if (number_of_urls==0):
+		return 0
+	else:
+		for i in range(number_of_urls):
+			x = input("urls")
+			urls.append(x)
+	return urls
+
+li=urlList()
+if(li!=0):
+	for links in li:
+		print(article(links))
+else:
+	print('no urls inputed')
--- scrap.py
+++ scrap.py
@@ -1,10 +1,8 @@
 from helper import *
 from urls import *
 li=urlList()
-# print(li)
-# articleDict={'author':author(),'Headline':headline(),'Description':description(),'Type':articleType(),'Claps':claps()}
 if(li!=0):
 	for links in li:
 		print(article(links))
-# else:
-# 	print('no urls inputed')
+else:
+	print('no urls inputed')
