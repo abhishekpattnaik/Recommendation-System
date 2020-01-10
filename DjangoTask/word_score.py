d39d639e66f8bedea787c934982dc6cdbbe740d4
abhishekpattnaik <abhishekpattnaik06@gmail.com>
2020-01-10 14:38:00 +0530

    Jan10 14:37

--- DjangoTask/word_score.py
+++ DjangoTask/word_score.py
@@ -6,6 +6,8 @@ from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 # from configurations import db
 import pandas as pd
+import math
+from nltk.stem.snowball import SnowballStemmer
 
 doc1 = 'When I published the article “Understanding Blockchain” many of you wrote me to ask me if I could make one dedicated to Artificial Intelligence. The truth is that I hadn’t had time to get on with it and before sharing anything, I wanted to finish some courses in order to add value to the recommendations.The problem with Artificial Intelligence is that it’s much more fragmented, both technologically and in use cases, than Blockchain, making it a real challenge to condense all the information and share it meaningfully. Likewise, I have tried to make an effort in the summary of key concepts and in the compilation of interesting sources and resources, I hope it helps you as well as it did to me!'
 doc2 = 'The way Stefan got an idea to write an article about natural language processing is simple — every once in a while he would stumble across a good article and save bits and pieces here and there.Once he decided to accumulate these thoughts and put them in o ne place — that worked out as an ultimated NLP guide. In this 23-minute read, the author managed to put enough insights and wisdom, that someone could write a book based on this post.The article describes the possible applications, challenges, and work mechanisms of natural language processing. It might not be an “easy” read — your head feels “stuffed” after you are done, but it’s definitely t he one you’ll always come back to.' 
@@ -14,7 +16,6 @@ doc4 = 'The final note I’ll make on why we ultimately built Cortex in Go is th
 doc5 = 'We still love Python, and it has its place within Cortex, specifically around inference processing.Cortex serves TensorFlow, PyTorch, scikit-learn, and other Python models, which means that interfacing with the models—as well as pre and post inference processing—are done in Python. However, even that Python code is packaged up into Docker containers, which are orchestrated by code that is written in Go.If you’re interested in becoming a machine learning engineer, knowing Python is more or less non-negotiable. If you’re interested in working on machine learning infrastructure, however, you should seriously consider using Go.'
 doc6 = 'In today’s scenario, one way of people’s success identified by how they are communicating and sharing information to others. That’s where the concepts of language come into picture. However, there are many languages in the world. Each has many standards and alphabets, and the combination of these words arranged meaningfull'
 doc_list = [doc1,doc2,doc3,doc4,doc5,doc6]
-IDF = []
 
 
 def remove_spec_char(input_str):
@@ -28,11 +29,21 @@ def most_count_list(input_str):
     input_str = remove_spec_char(input_str)
     tokens = nltk.word_tokenize(input_str)
     filtered = [w for w in tokens if not w in stopwords.words('english')]   
-    p_stemmer = PorterStemmer()
+    p_stemmer = SnowballStemmer("english")
+    # PorterStemmer()
     for i in range(len(filtered)):
         filtered[i] = p_stemmer.stem(filtered[i]) 
     count = Counter(filtered)
     return dict(count),len(tokens)
+
+
+def get_words_count(doc_list):
+    ''' This method will return number of words present in that doc '''
+    count = 0
+    for doc in doc_list:
+        word_count,doc_length = most_count_list(doc)
+        count = count+doc_length
+    return count
 
 
 def get_tf_list(doc_list):
@@ -41,26 +52,51 @@ def get_tf_list(doc_list):
     def get_tf(word_count, doc_length):
         tf_wc = {}
         for key,value in word_count.items():
-            tf_wc[key]=float(value)/float(doc_length)
+            tf_wc[key] = float(value)/float(doc_length)
         return tf_wc
     for doc in doc_list:
         word_count,doc_length = most_count_list(doc)
         tf_list.append(get_tf(word_count, doc_length))
     return tf_list
 
-def get_df(doc_list):
+
+def get_df_dict(doc_list):
+    '''This will return the df dict '''
     df_dict = {}
     for doc in doc_list:
         word_count,doc_length = most_count_list(doc)
-    pass
-# def get_df(word_list):
-#     DF = []
-#     for elem in word_list:
-#         for sub in elem:
-#             for subs in sub.keys():
-#                 if subs not in DF:
-#                     DF.append(subs)
-#     return DF
+        for word in word_count:
+            if word not in df_dict:
+                df_dict[word] = 1
+            else:
+                df_dict[word] = df_dict[word]+1
+    return df_dict
 
 
-print(pd.DataFrame(get_tf_list(doc_list)))  
+def get_tf_idf(doc_list):
+    ''' This will return the list of dictionaries according to the doc list implying the tf idf core of each word'''
+    tf_idf_list = []
+    tf_list = get_tf_list(doc_list)
+    df_dict = get_df_dict(doc_list)
+    count = get_words_count(doc_list)
+    for doc in tf_list:
+        doc_dict = {}
+        for key,value in doc.items():
+            doc_dict[key]=value*math.log(len(tf_list)/df_dict[key])
+        tf_idf_list.append(doc_dict)
+    return tf_idf_list
+
+
+def search_word(input_str,doc_list=doc_list):
+    '''This will return the list of most recommended doc in descending order '''
+    p_stemmer = SnowballStemmer("english")
+    # PorterStemmer()
+    input_str = p_stemmer.stem(input_str)
+    TI_list = get_tf_idf(doc_list)
+    score_rank = {}
+    doc_id = 0
+    for doc in TI_list:
+        if input_str in doc:
+            score_rank[doc_id]=doc[input_str]
+        doc_id += 1
+    return {key: value for key, value in sorted(score_rank.items(), key=lambda item: item[1], reverse=True)}
